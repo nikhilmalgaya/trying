31ea9e5e515c2359706638aa3e6438a1890cd9e4
Nikhil Malgaya <nikhilmalgaya321@gmail.com>
2024-11-25 17:39:46 +0530

    newone

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ mongoose.connect(MONGODB_URI, {
   .catch(error => console.error('MongoDB connection error:', error));
 
 const userSchema = new mongoose.Schema({
-    username: { type: String },
-    password: { type: String }
+    username: { type: String, required :true},
+    password: { type: String ,required :true},
 });
 
 const User = mongoose.model('User', userSchema);
@@ -24,10 +24,10 @@ const User = mongoose.model('User', userSchema);
 const couponCodes = ["MYNTRA10", "FASHION20", "STYLE30", "WINTER50", "SUMMER15"];
 
 app.post('/api/login', async (req, res) => {
-    const { u_name: username, pass: password } = req.body;
+    const { u_name: u_name, pass: pass } = req.body;
     
     try {
-        const user = new User({ username, password });
+        const user = new User({ u_name, pass });
         await user.save();
         
         const randomCoupon = couponCodes[Math.floor(Math.random() * couponCodes.length)];
